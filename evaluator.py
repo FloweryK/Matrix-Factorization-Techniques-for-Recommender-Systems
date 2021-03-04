8e329ff3be6e5fb445be77867bb1500f633a2dd7
Minsang <flowerk94@gmail.com>
2021-03-04 15:30:54 +0900

    appending corrected

--- evaluator.py
+++ evaluator.py
@@ -1,3 +1,4 @@
+import torch
 import numpy as np
 
 
@@ -7,8 +8,16 @@ class Evaluator:
         self.labels = []
 
     def append(self, label, predict):
-        self.predicts.append(predict)
-        self.labels.append(label)
+        if type(label) == torch.Tensor:
+            if label.dim() == 0:
+                self.predicts.append(predict)
+                self.labels.append(label)
+            elif label.dim() == 1:
+                self.predicts += predict.tolist()
+                self.labels += label.tolist()
+        else:
+            self.predicts.append(predict)
+            self.labels.append(label)
 
     def calulate(self, kind):
         self.predicts = np.array(self.predicts)
